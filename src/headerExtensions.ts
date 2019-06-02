f63b9939e7d4fd4c0cfdbd792d88ccc2f49f72da
Peter Vanco <petervanco@petervanco.sk>
2019-06-02 21:29:53 +0200

    Always include X- headers

--- src/headerExtensions.ts
+++ src/headerExtensions.ts
@@ -4,25 +4,26 @@ import * as path from "path";
 export const extensionHeaders = ['X-Image-Date', 'X-Image-Name'];
 
 export function addImageDateHeader(exifData: ExifData, headers: any) {
+    let xImageDate = '';
     if (!!exifData && !!exifData.exif && !!exifData.exif.CreateDate) {
         const dateAndTime = exifData.exif.CreateDate.split(' ');
-        return {
-            ...headers,
-            'X-Image-Date': `${dateAndTime[0].replace(/:/g, '.')} ${dateAndTime[1]}`
-        }
+        xImageDate = `${dateAndTime[0].replace(/:/g, '.')} ${dateAndTime[1]}`;
     }
-    return headers;
+    return {
+        ...headers,
+        'X-Image-Date': xImageDate
+    }
 }
 
 export function addImageNameHeader(rootFolder: string, file: string, headers: any) {
+    let xImageName = '';
     const rootFolderParts = rootFolder.split(path.sep);
     const fileParts = file.split(path.sep);
-
     if (fileParts.length > rootFolderParts.length) {
-        return {
-            ...headers,
-            'X-Image-Name': encodeURI(fileParts[rootFolderParts.length])
-        };
+        xImageName = encodeURI(fileParts[rootFolderParts.length]);
     }
-    return headers;
+    return {
+        ...headers,
+        'X-Image-Name': xImageName
+    };
 }
